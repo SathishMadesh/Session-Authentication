be620af0c18313f4b06adace57ce67e449695e77
SathishMadesh <s747272946@gmail.com>
2024-01-09 15:21:58 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import cookies from 'universal-cookie'
 
-function App() {
-  const [count, setCount] = useState(0)
+const cookies = new Cookies();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      username:"",
+      password:"",
+      error:"",
+      isauthenticated: false,
+    };
+  }
+
+  componentDidMount = () => {
+    this.getSession();
+  }
+
+  getSession = () => {
+    fetch("/api/session",{
+      credentials: "seme-rigin",
+    })
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data);
+      if(data.isauthenticated) {
+        this.setState({isauthenticated: true});
+      }else{
+        this.setState({isauthenticated: false});
+      }
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  whoami = () => {
+    fetch("/api/whoami", {
+      headers:{"content-Type":"application/json"},
+      credentials:"same-origin",
+    })
+    .then((res)=>res.json())
+    .then((data)=> {
+      console.log("You're logged in as:" + data.username);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
 }
 
-export default App
