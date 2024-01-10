7f85781a31fea7cbbe4338d828af9899e5d65b17
SathishMadesh <s747272946@gmail.com>
2024-01-10 15:59:27 +0530

    run build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -114,9 +114,38 @@ class App extends React.Component{
       return(
         <div className="container mt-3">
           <h1>React Cookie Auth</h1>
+          <br />
+          <h2>Login</h2>
+          <form onSubmit={this.login}>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" className='form-control' id='username' />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" className='form-control' id='password'
+              name='password'
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+              />
+              <div>
+                {this.state.error && <small className='text-danger'>
+                  {this.state.error}</small>}
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary">Login</button>
+          </form>
         </div>
       )
     }
+    return (
+      <div className="container-mt-3">
+        <h1>React Cookie Auth</h1>
+        <p>You are logged in!</p>
+        <button className="btn btn-primary-mr-2">WhoAmI</button>
+        <button className="btn btn-danger">Log Out</button>
+      </div>
+    )
 
   }
 
